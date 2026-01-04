a758beadf695629d84f28d461505ea12b8c20246
Diego Urquiza <dspaces1@gmail.com>
2026-01-04 12:21:21 -0500

    Initial Commit

---
+++ whatEat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whatEat
+//
+//  Created by Diego Urquiza on 1/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whatEat/whatEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatEatApp.swift
+//  whatEat
+//
+//  Created by Diego Urquiza on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct whatEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
